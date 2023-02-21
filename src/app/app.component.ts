eb997ec33f4d59b61b1469bcbf8f7b6727e29b44
Kevin Espinoza <kevinespinozadev@gmail.com>
2023-02-20 23:11:19 -0300

    Initial version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {FormGroup, FormBuilder, Validators} from '@angular/forms';
+import { SafeUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'qrcode-fast';
+export class AppComponent implements OnInit {
+
+  dataQR:string = '';
+  qrCodeDownloadLink : SafeUrl = '';
+  width:number = 256;
+  /* qrcode-fast.online */
+
+  constructor(
+    private formBuilder : FormBuilder
+  ){
+
+  }
+
+  form: FormGroup = this.formBuilder.group({
+    url: ['', Validators.required]
+  })
+
+
+  ngOnInit(): void {
+
+  }
+
+  generate():void{
+    this.dataQR = this.form.controls['url'].value;
+  }
+
+  reset():void{
+    this.dataQR = '';
+  }
+
+  onChangeURL(url: SafeUrl) {
+    this.qrCodeDownloadLink = url;
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { QRCodeModule } from 'angularx-qrcode';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    QRCodeModule,
+    FormsModule,
+    ReactiveFormsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
